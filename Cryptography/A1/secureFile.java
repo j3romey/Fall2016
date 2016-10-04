98b83a8d71c900278dddc3bd1b7a93c86fdd920f
ajadvien@ucalgary.ca <ajadvien@ucalgary.ca>
2016-10-04 07:37:00 -0600

    1st half of question 6

---
+++ Cryptography/A1/secureFile.java
@@ -0,0 +1,27 @@
+
+import java.io.*;
+import java.security.*;
+import javax.crypto.*;
+import javax.crypto.spec.*;
+import java.security.interfaces.*;
+import java.security.interfaces.DSAKey;
+import java.math.*;
+
+public class secureFile {
+	public static void main(String[] args) {
+		String filename = args[0];
+		String seed = args[1];
+		
+		byte[] dataBytes = "This is test data".getBytes();
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA");
+			md.update(dataBytes);
+			byte[] digest = md.digest();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+	
+	
+}
