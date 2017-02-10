9b657c7ec179382179bd7ff379d859f734f692d7
j3romey <jerome.adviento@hotmail.com>
2017-02-10 13:54:29 -0700

    hello

--- CPSC526/A2/proxy.py
+++ CPSC526/A2/proxy.py
@@ -1,13 +1,13 @@
 import time
 import socketserver
-import socket, threading
+import socket, threading, string
 import subprocess, sys, os, signal
 class MyTCPHandler(socketserver.BaseRequestHandler):
     BUFFER_SIZE = 4096
 
     def handle(self):
         logOptions = self.server.logOptions
-        
+
         open("temp", 'w')
         fw = open("temp", 'a')
 
@@ -17,24 +17,59 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         fw.write("Port logger running: srcPort=" + str(self.server.dstPort) + " host=" + self.server.server_name + "\n")
         ServAdd, ServPort = self.server.server_address
         fw.write("New connection:" + time.strftime("%c") + ",from:"  + ServAdd + "\n")
-        
         #creates thread to recieve
         def recv():
             while True:
                 data = client.recv(self.BUFFER_SIZE)
-                #print("from server" + data.decode("utf-8"))
-                    if(logOptions == "-raw"):
-                        print("raw")
-                    elif(logOptions == "-strip"):
-                        print("strip")
-                    elif("-hex"):
-                        print("hex")
-                    elif("-auto???"):
-                        print()
+                data_decode = data.decode("utf-8")
+                print("from server" + data.decode("utf-8"))
+                if(logOptions=="-raw"):
+                    print("raw")
+                elif(logOptions == "-strip"):
+                    data_printable = ""
+                    for char in data_decode:
+                        if not (char in string.printable):
+                            data_printable += "."
+                        else:
+                            data_printable += char
+                    fw.write("<---" +data_printable + '\n')
+                    fw.flush()
+                    print("strip")
+                elif(logOptions == "-hex"):
+                    print('derp')
+                    data_hex = ":".join("{:02x}".format(ord(c)) for c in data_decode)
+                    fw.write("<---" + data_hex + '\n')
+                    fw.flush()
+                    print(data_hex)
+                elif(logOptions.startswith('-auto')):
+                    nVal = int(logOptions[5:])
+                    def arSplit(l, n):
+                        n = max(1, n)
+                        return (l[i:i+n] for i in range(0, len(l), n))
+                    dSplit = arSplit(data, nVal)
+                    for vals in dSplit:
+                        outstring = ""
+                        counter = 0
+                        for byte in vals:
+                            if (byte == 92): # Backslash
+                                outstring += '\\\\'
+                            elif (byte == 9): # Tab
+                                outstring += '\\t'
+                            elif (byte == 10): # Newline
+                                outstring += '\\n'
+                            elif (byte == 13): # Carriage return
+                                outstring += '\\r'
+                            elif (32 <= byte <= 127):
+                                outstring += chr(byte)
+                            else:
+                                outstring += '\\' + str(hex(byte))[2:]
+                            counter += 1
+                        print(outstring + '\n')
+                        fw.write('<---' + outstring + '\n')
+                        fw.flush()
                 self.request.sendall(data)
-        
         threading.Thread(target=recv).start()
-        
+
         while 1:
             data = self.request.recv(self.BUFFER_SIZE)
             if len(data) == self.BUFFER_SIZE:
@@ -56,12 +91,14 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                         data_printable += "."
                     else:
                         data_printable += char
-                fw.write(data_printable)
+                fw.write("--->" +data_printable)
+                fw.flush()
                 print("strip")
             elif(logOptions == "-hex"):
                 data_hex = ":".join("{:02x}".format(ord(c)) for c in data_decode)
                 fw.write("--->" + data_hex + '\n')
-                print("-hex")
+                fw.flush()
+                print(data_hex)
             """data = data.decode( "utf-8")
             self.request.sendall( bytearray( "You said: " + data, "utf-8"))
             print("%s (%s) wrote: %s" % (self.client_address[0], 
@@ -71,8 +108,8 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             #client.send(bytearray(data, 'utf-8'))
             #print("from client" + data.decode ("utf-8"))
             client.send(data)
-            
-            
+
+
 
 if __name__ == "__main__":
     print (time.strftime("%m-%d-%Y--%H-%M-%S")) #Filename format
@@ -85,10 +122,10 @@ if __name__ == "__main__":
         server_name = sys.argv[argLen1-2]
         dstPort = int (sys.argv[argLen1-1])
     elif(len(sys.argv) == 5): #with log option  
-        logOptions = sys.argv[argLen-4]         
-        srcPort = int (sys.argv[argLen1-3])
-        server_name = sys.argv[argLen1-2]
-        dstPort = int (sys.argv[argLen1-1])
+        logOptions = sys.argv[argLen2-4]         
+        srcPort = int (sys.argv[argLen2-3])
+        server_name = sys.argv[argLen2-2]
+        dstPort = int (sys.argv[argLen2-1])
     else:
         print("Usage: [logOptions] <srcPort> <server> <dstPort>")
     #print(str(srcPort) + " " + server_name + " " + str(dstPort))
